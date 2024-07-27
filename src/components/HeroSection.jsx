0ba7cf6ab8608b2fa49d3090c242dac21f7b4771
aakash3435 <87906968+aakash3435@users.noreply.github.com>
2024-07-27 18:10:45 +0530

    save changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <section className="hero bg-gradient-to-r from-blue-500 to-blue-700 text-white">
       <div className="hero-content max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
         <h1 className="text-4xl sm:text-5xl font-bold mb-4">Welcome to GenzoAlpha Consulting Group</h1>
-        <p className="text-lg mb-4">"Transformation Meets Excellence"</p>
+        <p className="text-lg mb-4">"Where Transformation Inspires Excellent Results"</p>
         <p className="text-lg mb-8">To revolutionize the global workforce by seamlessly connecting talent and opportunity, fostering a culture of innovation, inclusivity, and unparalleled growth for businesses and professionals alike.</p>
         <button className="bg-white text-blue-500 hover:bg-blue-100 text-lg font-semibold py-2 px-4 rounded-full shadow-md transition duration-300">
           Learn More
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FiAlignJustify, FiSearch } from "react-icons/fi";
-// import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.png";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,35 +10,30 @@ const Header = () => {
 
   return (
     <header className="bg-white w-full top-0 fixed shadow-md px-6 py-3 text-black font-medium lg:px-14 z-50 ">
-      {/* // <header className="fixed top-0 w-full bg-white shadow-md p-6 text-black font-medium lg:px-14 z-50"> */}
+    
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-lg font-bold h-[80px] w-[80px] rounded-[40px] border border-black">
           <a href="/">
-            {/* <img
+            <img
               src={logo}
               alt="Logo"
               className="bg-cover h-[80px] w-[80px] rounded-[40px] border border-black-700"
-            /> */}
-            Logo
+            />
           </a>
         </div>
 
-        <nav className="hidden md:flex space-x-4 font-bold">
+        <nav className="hidden md:flex space-x-20 font-bold">
           <a href="/" className="hover:text-gray-300">
-            Home
+            Services
           </a>
           <a href="/mylearning" className="hover:text-gray-300">
-            My Learning
+           Pricing
           </a>
           <a href="/about" className="hover:text-gray-300">
-            About
+            About Us
           </a>
           <a href="/contact" className="hover:text-gray-300">
-            Contact
-          </a>
-
-          <a href="/search" className="hover:text-gray-300">
-            <FiSearch className="w-6 h-6 cursor-pointer hover:text-gray-300" />
+            Contact Us
           </a>
         </nav>
         <div className="md:hidden">
@@ -47,14 +42,10 @@ const Header = () => {
           </button>
         </div>
         {/* Search Icon */}
-        <div className="hidden md:block bg-[#F68B33] font-bold rounded-lg p-3 text-white">
-          {/* <FiSearch className="w-6 h-6 cursor-pointer hover:text-gray-300" /> */}
+        <div className="hidden md:block bg-blue-500 font-bold rounded-lg pl-8 pr-8 pt-2 pb-2 text-white">
           <nav>
-            <a href="/login" className="hover:text-gray-300  ">
-              Login /
-            </a>
-            <a href="/register" className="hover:text-gray-300">
-              Registration
+            <a href="/getstarted" className="hover:text-gray-300">
+              Get Started
             </a>
           </nav>
         </div>
@@ -62,26 +53,22 @@ const Header = () => {
       {isOpen && (
         <nav className="md:hidden bg-white shadow-md font-bold">
           <a href="/" className="block px-4 py-2 hover:bg-gray-200">
-            Home
+          Services
           </a>
           <a href="/mylearning" className="block px-4 py-2 hover:bg-gray-200">
-            My Learning
+          Pricing
           </a>
           <a href="/about" className="block px-4 py-2 hover:bg-gray-200">
-            About
+            About Us
           </a>
           <a href="/contact" className="block px-4 py-2 hover:bg-gray-200">
-            Contact
+            Contact Us
           </a>
-          <a href="/login" className="block px-4 py-2 hover:bg-gray-200">
-            Login
+         
+          <a href="/getstarted" className="block px-4 py-2 hover:bg-gray-200">
+          Get Started
           </a>
-          <a href="/register" className="block px-4 py-2 hover:bg-gray-200">
-            Registration
-          </a>
-          <a href="/search" className="block px-4 py-2 hover:bg-gray-200">
-            <FiSearch className="w-6 h-6 cursor-pointer hover:text-gray-300" />
-          </a>
+          
         </nav>
       )}
     </header>
